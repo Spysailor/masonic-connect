cceed46b1702f5fc051ff464bbb20acf0e33c12f
Spysailor <alexandrejm38@gmail.com>
2025-03-26 17:07:08 +0400

    Update App.tsx to wrap application in MobileFeatures component

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import MobileFooterMenu from "@/components/layout/MobileFooterMenu";
 import { AuthProvider } from "@/hooks/useAuth";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import PublicOnlyRoute from "@/components/PublicOnlyRoute";
+// Import du composant Mobile Features
+import MobileFeatures from "@/components/mobile/MobileFeatures";
 
 // Pages
 import Index from "./pages/Index";
@@ -58,181 +60,183 @@ const App = () => {
         <AuthProvider>
           <NotificationProvider>
             <TooltipProvider>
-              <Toaster />
-              <Sonner />
-              <AnimatePresence mode="wait">
-                <Routes>
-                  {/* Routes publiques */}
-                  <Route path="/" element={<PageWrapper><Index /></PageWrapper>} />
-                  <Route path="/login" element={
-                    <PageWrapper>
-                      <PublicOnlyRoute>
-                        <Login />
-                      </PublicOnlyRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/register" element={
-                    <PageWrapper>
-                      <PublicOnlyRoute>
-                        <Register />
-                      </PublicOnlyRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/join" element={
-                    <PageWrapper>
-                      <PublicOnlyRoute>
-                        <Join />
-                      </PublicOnlyRoute>
-                    </PageWrapper>
-                  } />
+              <MobileFeatures>
+                <Toaster />
+                <Sonner />
+                <AnimatePresence mode="wait">
+                  <Routes>
+                    {/* Routes publiques */}
+                    <Route path="/" element={<PageWrapper><Index /></PageWrapper>} />
+                    <Route path="/login" element={
+                      <PageWrapper>
+                        <PublicOnlyRoute>
+                          <Login />
+                        </PublicOnlyRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/register" element={
+                      <PageWrapper>
+                        <PublicOnlyRoute>
+                          <Register />
+                        </PublicOnlyRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/join" element={
+                      <PageWrapper>
+                        <PublicOnlyRoute>
+                          <Join />
+                        </PublicOnlyRoute>
+                      </PageWrapper>
+                    } />
 
-                  {/* Routes protégées (nécessitant une authentification) */}
-                  <Route path="/dashboard" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <Dashboard />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/freres" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <Members />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/freres/:id" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <MemberDetail />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/agenda" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <Agenda />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/agenda/create" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <TenueForm />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/agenda/:id/edit" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <TenueForm />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/agenda/:id" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <TenueDetail />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/planches" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <Planches />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/planches/:id" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <PlancheDetail />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/actualites" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <Actualites />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/actualites/create" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <NewsCreate />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/actualites/:id/edit" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <NewsEdit />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/actualites/:id" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <BibliothequeDetail />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/messages" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <Messages />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/bibliotheque" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <Bibliotheque />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/bibliotheque/:id" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <BibliothequeDetail />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/profile" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <Profile />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/notifications" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <Notifications />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/invitations" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <Invitations />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  <Route path="/loge-settings" element={
-                    <PageWrapper>
-                      <ProtectedRoute>
-                        <LogeSettings />
-                      </ProtectedRoute>
-                    </PageWrapper>
-                  } />
-                  
-                  {/* Si aucune route ne correspond, rediriger vers la page NotFound */}
-                  <Route path="*" element={<PageWrapper><NotFound /></PageWrapper>} />
-                </Routes>
-              </AnimatePresence>
-              <MobileFooterMenu />
+                    {/* Routes protégées (nécessitant une authentification) */}
+                    <Route path="/dashboard" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <Dashboard />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/freres" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <Members />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/freres/:id" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <MemberDetail />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/agenda" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <Agenda />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/agenda/create" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <TenueForm />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/agenda/:id/edit" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <TenueForm />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/agenda/:id" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <TenueDetail />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/planches" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <Planches />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/planches/:id" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <PlancheDetail />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/actualites" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <Actualites />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/actualites/create" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <NewsCreate />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/actualites/:id/edit" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <NewsEdit />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/actualites/:id" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <BibliothequeDetail />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/messages" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <Messages />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/bibliotheque" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <Bibliotheque />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/bibliotheque/:id" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <BibliothequeDetail />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/profile" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <Profile />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/notifications" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <Notifications />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/invitations" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <Invitations />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    <Route path="/loge-settings" element={
+                      <PageWrapper>
+                        <ProtectedRoute>
+                          <LogeSettings />
+                        </ProtectedRoute>
+                      </PageWrapper>
+                    } />
+                    
+                    {/* Si aucune route ne correspond, rediriger vers la page NotFound */}
+                    <Route path="*" element={<PageWrapper><NotFound /></PageWrapper>} />
+                  </Routes>
+                </AnimatePresence>
+                <MobileFooterMenu />
+              </MobileFeatures>
             </TooltipProvider>
           </NotificationProvider>
         </AuthProvider>
